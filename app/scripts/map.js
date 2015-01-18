a886292dbbecb1b301f8b07d548553e782bb6440
Robert Katzki <robert@katzki.de>
2015-01-18 01:29:46 +0100

    feat(map): use bounds to position marker

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -16,16 +16,21 @@ app.Map = new Model({
       zoom: 12
     });
 
-    this.initMarker();
+    this.initMarkers();
   },
 
   /**
    * Initialize the marker
    */
-  initMarker: function() {
-    this.markers = {};
+  initMarkers: function() {
+    var weddingLocation = [53.589882, 9.9851075],
+      weddingDummys = [[53.589882, 9.999], [53.589882, 9.984]],
+      partyLocation = [53.576456438929, 9.82718735933304],
+      partyDummys = [[53.576456438929, 9.821], [53.576456438929, 9.829]],
+      morningLocation = [53.560874, 10.03004],
+      morningDummys = [[53.560874, 10.04], [53.560874, 10.025]];
 
-    this.markers.wedding = L.marker([53.589882, 9.9851075], {
+    L.marker(weddingLocation, {
       alt: 'wedding',
       title: 'Standesamtliche Trauung',
       icon: L.mapbox.marker.icon({
@@ -35,7 +40,7 @@ app.Map = new Model({
     .addTo(this.mapCanvas)
     .on('click', this.onMarkerClick);
 
-    this.markers.party = L.marker([53.576456438929, 9.82718735933304], {
+    L.marker(partyLocation, {
       alt: 'party',
       title: 'Feier',
       icon: L.mapbox.marker.icon({
@@ -43,17 +48,23 @@ app.Map = new Model({
       })
     })
     .addTo(this.mapCanvas)
-    .on('click', this.onMarkerClick);
+    .on('click', this.onMarkerClick),
 
-    this.markers.morning = L.marker([53.560874, 10.03004], {
-      alt: 'morning',
-      title: 'Der Morgen danach…',
-      icon: L.mapbox.marker.icon({
-        'marker-color': '#f86767'
+    L.marker(morningLocation, {
+        alt: 'morning',
+        title: 'Der Morgen danach…',
+        icon: L.mapbox.marker.icon({
+          'marker-color': '#f86767'
+        })
       })
-    })
-    .addTo(this.mapCanvas)
-    .on('click', this.onMarkerClick);
+      .addTo(this.mapCanvas)
+      .on('click', this.onMarkerClick);
+
+    this.markerBounds = {
+      wedding: L.latLngBounds(weddingDummys),
+      party: L.latLngBounds(partyDummys),
+      morning: L.latLngBounds(morningDummys)
+    };
   },
 
   /**
@@ -62,14 +73,14 @@ app.Map = new Model({
    */
   onMarkerClick: function(event) {
     this.trigger('location', event.target.options.alt);
-    this.mapCanvas.setView(event.latlng, 15);
+    this.goToMarker(event.target.options.alt);
   },
 
   /**
    * Go to the passed in marker
-   * @param  {String} marker The marker name
+   * @param  {String} name The marker name
    */
-  goToMarker: function(marker) {
-    this.mapCanvas.setView(this.markers[marker]._latlng, 15);
+  goToMarker: function(name) {
+    this.mapCanvas.fitBounds(this.markerBounds[name]);
   }
 });
