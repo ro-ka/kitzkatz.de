494e0035de88317c630eebbf2020170ad5dd7683
Robert Katzki <robert@katzki.de>
2015-01-19 07:45:35 +0100

    feat(map): add marker tooltips

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -24,62 +24,52 @@ app.Map = new Model({
    */
   initMarkers: function() {
     var overviewDummys = [[53.560874, 10.04], [53.576456438929, 9.821]],
-      weddingLocation = [53.589882, 9.9851075],
       weddingDummys = [[53.589882, 9.999], [53.589882, 9.984]],
-      partyLocation = [53.576456438929, 9.82718735933304],
       partyDummys = [[53.576456438929, 9.82], [53.576456438929, 9.83]],
-      morningLocation = [53.560874, 10.03004],
       morningDummys = [[53.560874, 10.045], [53.560874, 10.027]],
-      sleepingLocation = [53.560498, 9.96019],
-      sleepingDummys = [[53.560498, 9.93], [53.560498, 9.97]];
+      sleepingDummys = [[53.560498, 9.93], [53.560498, 9.97]],
+      markers = [
+        {
+          name: 'wedding',
+          title: 'Standesamt',
+          symbol: 'circle-stroked',
+          location: [53.589882, 9.9851075]
+        },
+        {
+          name: 'party',
+          title: 'Feier',
+          symbol: 'circle-stroked',
+          location: [53.576456438929, 9.82718735933304]
+        },
+        {
+          name: 'morning',
+          title: 'Der Morgen danach…',
+          symbol: 'cafe',
+          location: [53.560874, 10.03004]
+        },
+        {
+          name: 'sleeping',
+          title: 'Übernachtung',
+          symbol: 'lodging',
+          location: [53.560498, 9.96019]
+        },
+      ];
 
-    L.marker(weddingLocation, {
-      alt: 'wedding',
-      title: 'Standesamtliche Trauung',
-      icon: L.mapbox.marker.icon({
-        'marker-size': 'large',
-        'marker-symbol': 'circle-stroked',
-        'marker-color': '#E74C3C'
-      })
-    })
-    .addTo(this.mapCanvas)
-    .on('click', this.onMarkerClick);
-
-    L.marker(partyLocation, {
-      alt: 'party',
-      title: 'Feier',
-      icon: L.mapbox.marker.icon({
-        'marker-size': 'large',
-        'marker-symbol': 'circle-stroked',
-        'marker-color': '#E74C3C'
-      })
-    })
-    .addTo(this.mapCanvas)
-    .on('click', this.onMarkerClick),
-
-    L.marker(morningLocation, {
-        alt: 'morning',
-        title: 'Der Morgen danach…',
-        icon: L.mapbox.marker.icon({
-        'marker-size': 'large',
-        'marker-symbol': 'cafe',
-        'marker-color': '#E74C3C'
+    $.each(markers, $.proxy(function(index, marker) {
+      L.marker(marker.location, {
+          alt: marker.name,
+          title: marker.title,
+          icon: L.mapbox.marker.icon({
+          'marker-size': 'large',
+          'marker-symbol': marker.symbol,
+          'marker-color': '#E74C3C'
+          })
         })
-      })
-      .addTo(this.mapCanvas)
-      .on('click', this.onMarkerClick);
-
-    L.marker(sleepingLocation, {
-        alt: 'sleeping',
-        title: 'Übernachtung',
-        icon: L.mapbox.marker.icon({
-        'marker-size': 'large',
-        'marker-symbol': 'lodging',
-        'marker-color': '#E74C3C'
-        })
-      })
-      .addTo(this.mapCanvas)
-      .on('click', this.onMarkerClick);
+        .addTo(this.mapCanvas)
+        .on('click', this.onMarkerClick)
+        .on('mouseover', this.onMarkerMouseover)
+        .on('mouseout', this.onMarkerMouseout);
+    }, this));
 
     this.markerBounds = {
       overview: L.latLngBounds(overviewDummys),
@@ -97,6 +87,24 @@ app.Map = new Model({
   onMarkerClick: function(event) {
     this.trigger('location', event.target.options.alt);
     this.goToMarker(event.target.options.alt);
+  },
+
+  /**
+   * On a marker hover
+   * @param  {Event} event The mouseover event
+   */
+  onMarkerMouseover: function(event) {
+    event.target
+      .bindPopup(event.target.options.title, {closeButton: false})
+      .openPopup();
+  },
+
+  /**
+   * On a marker unhover
+   * @param  {Event} event The mouseout event
+   */
+  onMarkerMouseout: function(event) {
+    event.target.closePopup();
   },
 
   /**
